689eab5f4bf5f4bb7e769696c845983451ca4230
protect-me <pmfwiww@gmail.com>
2021-07-27 23:31:56 +0900

    Feat: edit post signUp in users, install uuid

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const bcrypt = require('bcryptjs');
+// Encryption
+const bcrypt = require('bcryptjs'); // password
+const { v4: uuidv4 } = require('uuid') // uuid
+// mysql
 var mysqlConnection = require('../utils/mysqlConnection');
 var connection = mysqlConnection.getConnection()
 
@@ -10,33 +13,38 @@ router.get('/', function (req, res) {
     res.send(rows);
   });
 });
+
 router.post('/signUp', function (req, res) {
   const user = {
-    'userid': req.body.user.userid,
-    'name': req.body.user.name,
-    'password': req.body.user.password
+    'id': req.body.form.id,
+    'password': req.body.form.password,
   };
-  connection.query('SELECT userid FROM users WHERE userid = "' + user.userid + '"', function (err, row) {
-    if (row[0] == undefined) { //  동일한 아이디가 없을경우,
+  user.uuid = uuidv4()
+
+  connection.query(`SELECT id FROM users WHERE id = '${user.id}'`, function (err, row) {
+    if (row[0] == undefined) { //  동일한 아이디가 없을 경우,
       const salt = bcrypt.genSaltSync();
       const encryptedPassword = bcrypt.hashSync(user.password, salt);
-      connection.query('INSERT INTO users (userid,name,password) VALUES ("' + user.userid + '","' + user.name + '","' + encryptedPassword + '")', user, function (err, row2) {
+      connection.query(
+        `INSERT INTO users (uuid, id, password) 
+          VALUES ('${user.uuid}', '${user.id}', '${encryptedPassword}')
+        `, function (err, row2) {
         if (err) throw err;
       });
       res.json({
         success: true,
-        message: 'Sing Up Success!'
+        message: 'Sign Up Success! 🧙🏻‍♂️'
       })
     }
     else {
       res.json({
         success: false,
-        message: 'Sign Up Failed Please use anoter ID'
+        message: 'Sign Up Failed. Please use anoter ID 🧙🏻‍♂️'
       })
     }
   });
-
 });
+
 router.post('/login', function (req, res) {
   const user = {
     'userid': req.body.user.userid,
@@ -46,7 +54,7 @@ router.post('/login', function (req, res) {
     if (err) {
       res.json({ // 매칭되는 아이디 없을 경우
         success: false,
-        message: 'Login failed please check your id or password!'
+        message: 'Login failed please check your id or password! 🧙🏻‍♂️'
       })
     }
     if (row[0] !== undefined && row[0].userid === user.userid) {
@@ -54,12 +62,13 @@ router.post('/login', function (req, res) {
         if (res2) {
           res.json({ // 로그인 성공 
             success: true,
-            message: 'Login successful!'
+            message: 'Login successful! 🧙🏻‍♂️'
           })
         }
         else {
-          res.json({ // 매칭되는 아이디는 있으나, 비밀번호가 틀린 경우            success: false,
-            message: 'Login failed please check your id or password!'
+          res.json({ // 매칭되는 아이디는 있으나, 비밀번호가 틀린 경우
+            success: false,
+            message: 'Login failed please check your id or password! 🧙🏻‍♂️'
           })
         }
       })
